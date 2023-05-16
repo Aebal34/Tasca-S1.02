be6071121152cb324e8744048e62d2915d7dc503
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-16 14:39:54 +0200

    EmptySaleException applied to sale method

--- n1exercici1/Product.java
+++ n1exercici1/Product.java
@@ -7,4 +7,18 @@ public class Product {
 	private String name;
 	private int price;
 	
+	//CONSTRUCTOR
+	
+	public Product(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	//GETTERS
+	
+	public String getName() {
+		return name;
+	}
+	public int getPrice() {
+		return price;
+	}
 }
--- n1exercici1/Sale.java
+++ n1exercici1/Sale.java
@@ -7,4 +7,26 @@ public class Sale {
 	private int totalPrice;
 	private ArrayList<Product> products;
 	
+	//CONSTRUCTOR
+	
+	public Sale() {
+		
+	}
+	
+	//METHODS
+	
+	public void calculateTotal() throws EmptySaleException {
+		
+		if(products.size()==0) {
+			throw new EmptySaleException("Para realizar una venta, primero añada productos.");
+		}else {
+			for(int i=0; i<products.size(); i++) {
+				
+				totalPrice += products.get(i).getPrice();
+			}
+		}
+	}
+	public void addProduct(Product product) {
+		products.add(product);
+	}
 }
