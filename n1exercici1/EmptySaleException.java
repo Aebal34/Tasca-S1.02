79bcd66fc1123fb7dd76981cf5a5c573707fd10d
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-16 14:39:25 +0200

    EmptySaleException created

---
+++ n1exercici1/EmptySaleException.java
@@ -0,0 +1,11 @@
+package n1exercici1;
+
+public class EmptySaleException extends Exception{
+
+	public EmptySaleException() {
+		
+	}
+	public EmptySaleException(String message) {
+		super(message);
+	}
+}
