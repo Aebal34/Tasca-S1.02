c15789b4f82bcffbf1c94b1b7d108e912e0cb20c
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-16 18:56:34 +0200

    Seat class done

--- n3exercici1/App.java
+++ n3exercici1/App.java
@@ -3,7 +3,7 @@ package n3exercici1;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- n3exercici1/Seat.java
+++ n3exercici1/Seat.java
@@ -2,4 +2,41 @@ package n3exercici1;
 
 public class Seat {
 
+	//ATTRIBUTES
+	private int row;
+	private int seat;
+	private String person;
+	
+	//CONSTRUCTOR
+	public Seat(int row, int seat, String person) {
+		
+		this.row=row;
+		this.seat=seat;
+		this.person=person;
+	}
+	
+	//GETTERS
+	public int getRow() {
+		return row;
+	}
+	public int getSeat() {
+		return seat;
+	}
+	public String getPerson() {
+		return person;
+	}
+	
+	//METHODS
+	
+	public boolean equals() {
+		return row == seat;
+	}
+
+	@Override
+	public String toString() {
+		return "Row: "+row+", Seat: "+seat+", Person: "+person;
+	}
+	
+	
+	
 }
