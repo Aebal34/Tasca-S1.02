313a08bda64222411bcacf138b780a6cc3a1f343
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-16 14:17:48 +0200

    Exercise start

---
+++ n1exercici1/Product.java
@@ -0,0 +1,10 @@
+package n1exercici1;
+
+public class Product {
+
+	//ATTRIBUTES
+	
+	private String name;
+	private int price;
+	
+}
---
+++ n1exercici1/Sale.java
@@ -0,0 +1,10 @@
+package n1exercici1;
+
+import java.util.ArrayList;
+
+public class Sale {
+
+	private int totalPrice;
+	private ArrayList<Product> products;
+	
+}
