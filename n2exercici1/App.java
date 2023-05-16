366580b432aae0a5eb4e185a7a941feb4dac51b6
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-16 15:40:41 +0200

    Input.ReadByte done

---
+++ n2exercici1/App.java
@@ -0,0 +1,14 @@
+package n2exercici1;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		//Input.readByte test
+		System.out.println("Tiene usted..."+Input.readByte("Introduzca su edad.")+" años");
+		
+		
+		
+	}
+
+}
---
+++ n2exercici1/Input.java
@@ -0,0 +1,31 @@
+package n2exercici1;
+
+import java.util.*;
+public class Input {
+
+	static Scanner input = new Scanner(System.in);
+	
+	public static byte readByte(String message) {
+		
+		boolean correct = false;
+		byte finalByte = 0;
+		
+		//It will keep asking for a byte until the type of data is correctly typed.
+		do {
+			try {
+				
+			System.out.println(message);
+			finalByte = input.nextByte();
+			correct = true;
+			
+			}catch(InputMismatchException e) {
+				System.out.println("El tipo de dato introducido no es correcto.");
+				input.nextLine(); //We clean the buffer so we can introduce the data again.
+			}
+		}while(!correct);
+			
+		return finalByte;
+	}
+	
+	
+}
